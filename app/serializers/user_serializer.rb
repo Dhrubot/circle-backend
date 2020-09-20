af163631a76d0e8daf13ba89b54f7b2a9d4343f3
Dhrubot <dhrubo.dh@gmail.com>
2020-09-19 20:11:52 -0400

    added attribites to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email
 
-  has_many :authored_posts
-  has_many :comments
-  has_many :commented_posts
   has_many :followers
   has_many :followings
 end
