a021c65d0d8aa081decfdc635b099bd9f982dbed
Dhrubot <dhrubo.dh@gmail.com>
2020-09-19 20:11:35 -0400

    added attribites to post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :location
-  belongs_to :author
+  attributes :id, :title, :description, :location, :author
   has_many :comments
   has_many :likes
+
+  def author
+    UserSerializer.new(object.author)
+  end
 end
