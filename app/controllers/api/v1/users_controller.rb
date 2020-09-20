446600182a48c53b147e5827a4965dde2f6d6399
Dhrubot <dhrubo.dh@gmail.com>
2020-09-19 20:09:10 -0400

    Added the ability to create a new user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,17 +10,21 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render json: @user, include: ['followers.follower', 'followings.following']
+    render json: @user
   end
 
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: @user, status: :created, location: @user
+      session[:user_id] = @user.id
+
+      render json: @user, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
     end
     
   end
@@ -47,6 +51,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 end
