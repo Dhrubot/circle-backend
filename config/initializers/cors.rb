a38fab1ab4917706ca443741db30787ec2f11bb5
Dhrubot <dhrubo.dh@gmail.com>
2020-09-28 17:09:29 -0400

    experiemnting with heroku origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins '*'
 
-    resource 'https://circle-dh.herokuapp.com/api/v1',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
