7caf5d1f891af09a556417526fa049712dfa1101
Dhrubot <dhrubo.dh@gmail.com>
2020-10-02 21:22:50 -0400

    Added origin github address

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://circle-dh.herokuapp.com/'
+    origins 'https://dhrubot.github.io/circle-frontend/'
 
     resource '*',
       headers: :any,
