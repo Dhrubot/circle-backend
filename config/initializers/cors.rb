fc2c9a3eaa094d1f05a9aabb4bfd05a073ab08a3
Dhrubot <dhrubo.dh@gmail.com>
2020-10-03 05:12:41 -0400

    Removed backslash

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://dhrubot.github.io/circle-frontend/'
+    origins 'https://dhrubot.github.io/circle-frontend'
 
     resource '*',
       headers: :any,
