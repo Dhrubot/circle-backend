124d3bdf3a3c64eae9a41fe75a9d38310e36bfaf
Dhrubot <dhrubo.dh@gmail.com>
2020-10-02 21:11:49 -0400

    Fixed CORS issues credentials

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://circle-dh.herokuapp.com/'
 
     resource '*',
       headers: :any,
